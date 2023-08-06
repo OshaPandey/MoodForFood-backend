2bab23c50fbc3f07ef6d82e82bdd1212fb0937e5
Osha Pandey <osha.pandey8@gmail.com>
2023-08-06 11:24:17 +0530

    initial committ

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const cors =require("cors");
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT||5000;
 const connectToDB = require("./db");
-
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
